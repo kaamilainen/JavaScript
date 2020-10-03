53f67b0cfd7c3b82c1b552c90862a467a975083d
Tiina <example@gmail.com>
2020-10-03 21:10:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+//console.log(1);
+"use strict";//modern mode, not old, new version of coding
+/*LESSON 10
+
+
+
+/*LESSON 9
+console.log('arr' + " - object");
+console.log( 4 + " - object");
+console.log( 4 + +" - object");
+console.log( 4 + +" 5");
+*/
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+
+
+
+/*LESSON 8
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Hi, ${user}`);
+*/
+
+
+
+/*LESSON 7
+//const result = confirm("Are you here?");
+//console.log(result);
+
+
+/*const answer = +prompt("Are you 18?", "");//+ преобразует в стринг
+console.log(answer);
+console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt("Your name", "");
+answers[1] = prompt("Your surname", "");
+answers[2] = prompt("Your age", "");
+
+document.write(answers);/*
+
+
+
+
+
+
+/*
+
+let number = 5; //can be changed
+const leftBorderWidth = 1; //constanta, no changes
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a:50
+};
+
+obj.a = 10;
+
+
+var name = "Ivan";
+
+const obj = {
+    name: 'Ivan',
+    age: 25,
+    isMarried: false
+
+};
+
+console.log(obj);
+console.log(obj['name']);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', [], {}];
+console.log(arr[2]);*/
