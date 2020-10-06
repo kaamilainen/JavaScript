4896c50d7bb029a56f0480199509c622ee65c55e
Tiina <example@gmail.com>
2020-10-06 19:37:06 +0300

    New task

--- tasks.js
+++ tasks.js
@@ -1,8 +1,6 @@
 
 const numberOfFilms = +prompt('How many films you have seen?', '');
 
-
-
 const personalMovieDB =  {
     count: numberOfFilms,
     movies: {},
@@ -11,5 +9,15 @@ const personalMovieDB =  {
     privat: false
 };
 
+const a = prompt('What is the last one?', ''),
+      b = prompt('How would you rate it?', ''),
+      c = prompt('What is the last one?', ''),
+      d = prompt('How would you rate it?', '');
+      
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
 
