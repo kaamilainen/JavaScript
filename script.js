e35600b0612350abcefeeddb1a211a024ab74ab2
Tiina <example@gmail.com>
2020-10-06 19:55:21 +0300

    Lesson 13

--- script.js
+++ script.js
@@ -10,14 +10,16 @@ console.log( 4 + " - object");
 console.log( 4 + +" - object");
 console.log( 4 + +" 5");
 */
+
+
 let incr = 10,
     decr = 10;
 
 incr++;
 decr--;
 
-console.log(incr);
-console.log(decr);
+//console.log(incr);
+//console.log(decr);
 
 
 
--- tasks.js
+++ tasks.js
@@ -1,4 +1,47 @@
+"use strict";
 
+
+
+
+
+/*TASK 2
+if (4 == 9){
+    console.log("ok");
+} else{
+    console.log("nope");
+    }
+
+
+var num = 50;
+if (num < 49){
+    console.log("error");
+}else if (num > 100){
+         console.log("too much");
+        }else{
+            console.log("ok");
+            }
+
+
+
+(num === 50) ? console.log("ok") : console.log("error");
+//condition  if {               }else{                  }
+
+const a = 51;
+switch (a) {
+    case 49: console.log("wrong");
+        break;
+    case 100: console.log("wrong");
+        break;
+    case 50: console.log("correct");
+        break;
+    default: console.log("next time");
+        break;
+}*/
+
+
+
+
+/*TASK 1
 const numberOfFilms = +prompt('How many films you have seen?', '');
 
 const personalMovieDB =  {
@@ -18,6 +61,4 @@ const a = prompt('What is the last one?', ''),
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
-
-
+console.log(personalMovieDB);*/
