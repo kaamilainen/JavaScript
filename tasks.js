e338a99f0fadccb15b365d9e3ae18ef38eda7e74
Tiina <example@gmail.com>
2020-10-06 19:23:28 +0300

    Node deleted

---
+++ tasks.js
@@ -0,0 +1,15 @@
+
+const numberOfFilms = +prompt('How many films you have seen?', '');
+
+
+
+const personalMovieDB =  {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
