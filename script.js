34ffc915a9fa78f48580868b1e89e0c1348e26f6
Tiina <example@gmail.com>
2020-10-07 20:04:52 +0300

    7.10.20

--- script.js
+++ script.js
@@ -1,5 +1,252 @@
 //console.log(1);
 "use strict";//modern mode, not old, new version of coding
+/*LESSON 20*/
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+options.makeTest();
+
+console.log(Object.keys(options));//получаем массив со всеми ключами
+console.log(Object.keys(options).length);
+
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+
+// let counter = 0;
+
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object'){
+//         for (let i in options[key]){
+//             console.log(`Feature ${i} has value ${options[key][i]}`);
+//             counter++;
+//         }
+//     }else{
+//         console.log(`Feature ${key} has value ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+
+
+
+
+
+
+/*LESSON 19*/
+
+// function first() {
+//     //Do something
+//     setTimeout(function(){
+//         console.log(1);
+//     }, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback){
+//     console.log(`I learn ${lang}`);
+//     callback();
+// }
+
+// // learnJS('JavaScript', function(){
+// //     console.log('I have passed');
+// // });
+// function done(){
+//     console.log('I have passed');
+// }
+
+// learnJS('JavaScript', done);
+
+
+
+
+
+/*LESSON 18*/
+
+//const arr = [1, 2, 3];
+//console.log(arr.length);
+
+// const str = "test";
+
+// console.log(str.length);
+// console.log(str[2]);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf("fruit"));
+
+
+// const logg = "hello world";
+// console.log(logg.slice(6, 11));
+// console.log(logg.slice(-5, -1));
+
+// console.log(logg.substring(6, 11));
+
+// console.log(logg.substr(6, 5));//второй аргумент это длинна
+
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+
+
+
+/*LESSON 15*/
+
+// let num = 20;
+
+// function showFirstMessage(text, argument){
+//     console.log(text);
+//     num = 20;
+// }
+
+// showFirstMessage("Hello");
+// console.log(num);
+
+
+
+
+
+
+// function calc(a, b){
+//     return ( a + b );
+// }
+// console.log(calc(4, 3));
+// console.log(calc(5, 4));
+// console.log(calc(9, 6));
+
+
+
+
+
+
+// function ret(){
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+
+
+
+
+// const logger = function() {
+//     console.log("hello");
+// };
+
+// logger();
+
+
+
+// const calculator = (a, b) => { return a + b };
+
+
+/*LESSON 14
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+//
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+//
+let number = 45;
+
+for (let i = 1; i < 8; i++) {
+    console.log(number);
+    number--;
+}
+
+for (let i = 1; i < 8; i++) {
+    if (i === 3){
+        break;
+        //continue;
+    }
+    console.log(i);
+    
+}*/
+
+
+
+
+/*TASK 13
+if (4 == 9){
+    console.log("ok");
+} else{
+    console.log("nope");
+    }
+
+
+var num = 50;
+if (num < 49){
+    console.log("error");
+}else if (num > 100){
+         console.log("too much");
+        }else{
+            console.log("ok");
+            }
+
+
+
+(num === 50) ? console.log("ok") : console.log("error");
+//condition  if {               }else{                  }
+
+const a = 51;
+switch (a) {
+    case 49: console.log("wrong");
+        break;
+    case 100: console.log("wrong");
+        break;
+    case 50: console.log("correct");
+        break;
+    default: console.log("next time");
+        break;
+}*/
+
+
+
+
+
+
+
+
+
 /*LESSON 10
 
 
--- tasks.js
+++ tasks.js
@@ -1,79 +1,133 @@
 "use strict";
+// //TASK 4
 
 
 
 
 
-/*TASK 3
-let num = 50;
-
-while (num <= 55) {
-    console.log(num);
-    num++;
-}
-
-//
-
-do {
-    console.log(num);
-    num++;
-}
-while (num < 55);
-
-//
-let number = 45;
-
-for (let i = 1; i < 8; i++) {
-    console.log(number);
-    number--;
-}
-
-for (let i = 1; i < 8; i++) {
-    if (i === 3){
-        break;
-        //continue;
-    }
-    console.log(i);
-    
-}*/
 
 
 
+// //TASK 3
 
-/*TASK 2
-if (4 == 9){
-    console.log("ok");
-} else{
-    console.log("nope");
-    }
+// let numberOfFilms;
+
+// function start() {
+//     numberOfFilms = +prompt('How many films you have seen?', '');
+
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('How many films you have seen?', '');
+//     }
+// }
+// //start();
+
+// const personalMovieDB =  {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
-var num = 50;
-if (num < 49){
-    console.log("error");
-}else if (num > 100){
-         console.log("too much");
-        }else{
-            console.log("ok");
-            }
+// // function showMyDB(hidden) {
+// //     if (!hidden){
+// //         console.log(personalMovieDB);
+// //     }
+// // }
+// //showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for (let i = 1; i < 4; i++){
+//         let question = prompt(`What is your favourite genres number ${i}?`, '');
+//         personalMovieDB.genres[i-1] = question;
+//         //personalMovieDB.genres[i-1] = prompt(`What is your favourite genres number ${i}?`, '');
+//         //second is faster
+        
+//     }
+// }
+// writeYourGenres();
+
+
+// function rememberMyFilms(){
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('What is the last one?', ''),
+//               b = prompt('How would you rate it?', '');
+              
+//               if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//                     personalMovieDB.movies[a] = b;
+//                     console.log('done');
+//                 } else {
+//                     console.log('error');
+//                     i--;
+//                 }   
+//     }
+// }
+// //rememberMyFilms();
+
+// function detectPersonalLevel(){
+//     if (numberOfFilms <= 10) {
+//         prompt("You have watched a few films");
+//     } else if (numberOfFilms > 10 && numberOfFilms < 30) {
+//         prompt("You doing well");
+//     } else if (numberOfFilms >= 30) {
+//         prompt('You are a cinemamen!');
+//     } else {
+//         console.log('error');
+//     }
+// }
+// //detectPersonalLevel();
+
+// console.log(personalMovieDB);
 
 
 
-(num === 50) ? console.log("ok") : console.log("error");
-//condition  if {               }else{                  }
 
-const a = 51;
-switch (a) {
-    case 49: console.log("wrong");
-        break;
-    case 100: console.log("wrong");
-        break;
-    case 50: console.log("correct");
-        break;
-    default: console.log("next time");
-        break;
-}*/
 
+
+
+
+//TASK 2
+
+//const numberOfFilms = +prompt('How many films you have seen?', '');
+//const age = +prompt("how old are you", "");
+
+// const personalMovieDB =  {
+//     count: numberOfFilms,
+//     //age: age,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+//personalMovieDB.age = age;
+
+// if (numberOfFilms <= 10) {
+//     prompt("You have watched a few films");
+// } else if (numberOfFilms > 10 && numberOfFilms < 30) {
+//             prompt("You doing well");
+//         } else if (numberOfFilms >= 30) {
+//             prompt('You are a cinemamen!');
+//             } else {
+//                 console.log('error');
+//             }
+
+// for (let i = 0; i < 1 /*i<2*/; i++) {
+//     const a = prompt('What is the last one?', ''),
+//           b = prompt('How would you rate it?', '');
+//           //c = prompt('actor first name', ''),
+//           //d = prompt('actor family name', '');
+//           if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//                 personalMovieDB.movies[a] = b;
+//                 //personalMovieDB.actors[c] = d;
+//                 console.log('done');
+//             } else {
+//                 console.log('error');
+//                 i--;
+//             }   
+// }
+
+// console.log(personalMovieDB);
 
 
 
